e3835b821470a9659e664139305aae23ed57e6a0
Jesse <ahlbrecht.jesse@icloud.com>
2022-07-16 15:53:19 +0200

    Added Gstreamer to load webcam

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -255,12 +255,41 @@ class LoadImages:
 
 
 class LoadWebcam:  # for inference
+
+    def gstreamer_pipeline(
+        capture_width=1920,
+        capture_height=1080,
+        display_width=960,
+        display_height=540,
+        framerate=30,
+        flip_method=0,
+    ):
+        return (
+            "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink drop=True"
+            % (
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+                display_width,
+                display_height,
+            )
+        )
+     
     # YOLOv5 local webcam dataloader, i.e. `python detect.py --source 0`
     def __init__(self, pipe='0', img_size=640, stride=32):
         self.img_size = img_size
         self.stride = stride
         self.pipe = eval(pipe) if pipe.isnumeric() else pipe
-        self.cap = cv2.VideoCapture(self.pipe)  # video capture object
+        self.cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)#cv2.VideoCapture(self.pipe)
+    
+    # video capture object
         self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)  # set buffer size
 
     def __iter__(self):
